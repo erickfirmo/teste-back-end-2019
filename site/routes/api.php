1dba3caedb54e013d321063b62092b98f9eeeb63
Érick Firmo <erickluques@gmail.com>
2020-06-19 00:26:41 -0300

    Update api.php

--- site/routes/api.php
+++ site/routes/api.php
@@ -12,7 +12,19 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*/
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+Route::get('products', 'Api\\ProductController@index');
+
+Route::get('products/store', 'Api\\ProductController@store');
+
+Route::get('products/{id}', 'Api\\ProductController@show');
+
+Route::put('products/{id}/update', 'Api\\ProductController@update');
+
+Route::delete('products/{id}/destroy', 'Api\\ProductController@destroy');
+
