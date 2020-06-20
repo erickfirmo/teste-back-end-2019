f02a2cfdcbed2bb11d1915326558bf1021a85efd
Érick Firmo <erickluques@gmail.com>
2020-06-20 04:13:20 -0300

    Update api.php

--- site/routes/api.php
+++ site/routes/api.php
@@ -18,30 +18,30 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 */
 
-Route::group(['middleware' => ['apiJwt']], function(){
-    
-    Route::get('users', 'Api\UserController@index');
-
-    Route::get('products', 'Api\ProductController@index');
-
-    Route::get('products/{id}', 'Api\ProductController@show');
-
-    Route::post('products/store', 'Api\ProductController@store');
-
-    Route::put('products/{id}/update', 'Api\ProductController@update');
-
-    Route::delete('products/{id}/destroy', 'Api\ProductController@destroy');
-});
 
 
 
-
-
-/* Auth routes */
-
+// auth routes
 Route::prefix('auth')->group(function () {
     Route::post('login', 'Api\AuthController@login');
+    Route::post('refresh', 'Api\AuthController@refresh');
+    Route::post('me', 'Api\AuthController@me');
 
 });
 
+Route::group(['middleware' => ['apiJwt']], function(){
+
+    // auth routes
+    Route::prefix('auth')->group(function () {
+        Route::post('logout', 'Api\AuthController@logout');
+    
+    });
+
+    Route::get('users', 'Api\UserController@index');
+    Route::get('products', 'Api\ProductController@index')->name('api.products');
+    Route::get('products/{id}', 'Api\ProductController@show');
+    Route::post('products/store', 'Api\ProductController@store');
+    Route::put('products/{id}/update', 'Api\ProductController@update');
+    Route::delete('products/{id}/destroy', 'Api\ProductController@destroy');
+});
 
