3cdee58d48c1e87bb83cfc822355bfc9c7e821b7
Érick Firmo <erickluques@gmail.com>
2020-06-20 01:20:58 -0300

    Update AuthController.php

--- site/app/Http/Controllers/Api/AuthController.php
+++ site/app/Http/Controllers/Api/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\AuthRequest;
+use App\Http\Requests\Api\AuthRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use JWTAuth;
