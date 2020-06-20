f08b3670c2c5298edfe2f894cd47b747e005b056
Érick Firmo <erickluques@gmail.com>
2020-06-19 21:56:54 -0300

    Create UsersTableSeeder.php

---
+++ site/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new App\User;
+        $user->name = 'Érick Firmo';
+        $user->email = 'erickluques@gmail.com';
+        $user->password = 'secret';
+        $user->save();
+    }
+}
