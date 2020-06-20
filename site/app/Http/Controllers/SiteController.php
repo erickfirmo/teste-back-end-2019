85527cf719f846759fdff7c76aa14f4181ddc709
Érick Firmo <erickluques@gmail.com>
2020-06-20 04:16:24 -0300

    Update SiteController.php

--- site/app/Http/Controllers/SiteController.php
+++ site/app/Http/Controllers/SiteController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function doc()
+    {
+        return view('doc');
+    }
+    
 }
