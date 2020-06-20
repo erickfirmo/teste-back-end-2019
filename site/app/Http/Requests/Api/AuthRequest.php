f4a69a60a1c02c9a8344356b1bb3b7487c4cc168
Érick Firmo <erickluques@gmail.com>
2020-06-20 01:27:30 -0300

    Update AuthRequest.php

--- site/app/Http/Requests/Api/AuthRequest.php
+++ site/app/Http/Requests/Api/AuthRequest.php
@@ -24,7 +24,7 @@ class AuthRequest extends ApiFormRequest
     public function rules()
     {
         return [
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255',
             'password' => 'required|string|min:6'
         ];
         
