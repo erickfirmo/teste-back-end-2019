de5f7cde9e2cede271befb9c7c926f1e6693a08c
Érick Firmo <erickluques@gmail.com>
2020-06-20 00:28:14 -0300

    Create AuthRequest.php

---
+++ site/app/Http/Requests/AuthRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email é obrigatório!',
+            'password.required' => 'Senha é obrigatória!'
+        ];
+    }
+}
