6fb7b01955fca8304c784fe9af4e2e66f6841baf
Érick Firmo <erickluques@gmail.com>
2020-06-21 02:11:39 -0300

    Update ProductRequest.php

--- site/app/Http/Requests/Api/ProductRequest.php
+++ site/app/Http/Requests/Api/ProductRequest.php
@@ -3,8 +3,9 @@
 namespace App\Http\Requests\Api;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Api\ApiFormRequest;
 
-class ProductRequest extends FormRequest
+class ProductRequest extends ApiFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,10 +25,12 @@ class ProductRequest extends FormRequest
     public function rules()
     {
         return [
+            
             'name' => 'required|string|min:2|max:200|unique:products',
-            'price' => 'required|numeric|between:XXXX',
-            'weight' => 'required|numeric|min:1|max:999999',
-            'size' => 'required|string|x,y,z'
+            //'price' => 'required|numeric|between:XXXX',
+            //]'weight' => 'required|numeric|min:1|max:999999',
+            //'size' => 'required|string|x,y,z'
+            
         ];
     }
 }
